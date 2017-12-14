357c02c0d203a41c42962c98e5f2a8ff2d754e51
Rick von Buelow <Rick von Buelow>
2017-12-14 15:45:41 -0800

    adding sunburst animation interactivity

--- src/js/views/Sunburst.js
+++ src/js/views/Sunburst.js
@@ -6,12 +6,19 @@ import d3zoom from "d3-zoom";
 import util from "../util";
 import SunburstTemplate from "./sunburst.html";
 import BaseChart from "./BaseChart";
-import dataSunburst from "../data/sunburst";
+// import dataSunburst from "../data/sunburst";
 
 let Sunburst = BaseChart.extend({
   initialize: function (options) {
-    this.data = dataSunburst;
+    // this.data = dataSunburst;
     this.parentEl = options.parentEl;
+
+    this.addListeners();
+    this.initVars();
+    this.createSvg();
+    this.loadDataNBA(options.data);
+  },
+  initVars: function () {
     this.margin = {top: 20, right: 20, bottom: 35, left: 50, textTop: 15};
     this.size = this.setSize();
     this.size.w = 500;
@@ -22,10 +29,7 @@ let Sunburst = BaseChart.extend({
       name: "Teams",
       children:[]
     };
-    // console.log("SIZE:", this.size);
-    this.addListeners();
-    this.createSvg();
-    this.loadDataNBA(options.data);
+    this.color_palettes = [['#4abdac', '#fc4a1a', '#f7b733'], ['#f03b20', '#feb24c', '#ffeda0'], ['#007849', '#0375b4', '#ffce00'], ['#373737', '#dcd0c0', '#c0b283'], ['#e37222', '#07889b', '#eeaa7b'], ['#062f4f', '#813772', '#b82601'], ['#565656', '#76323f', '#c09f80']];
   },
   loadDataNBA: function (data) {
     let self = this;
@@ -44,8 +48,8 @@ let Sunburst = BaseChart.extend({
 
     let peopleArr = arr.map( (obj)=> {
       return {
-        name: obj.player ? obj.player : obj.coachName,
-        size: 30
+        name: obj.player ? obj.player.split(" ")[1] : obj.lastName,
+        size: 5
        };
     });
 
@@ -58,7 +62,7 @@ let Sunburst = BaseChart.extend({
       return {
         name: name,
         children: self.getVal(val),
-        size: 20
+        size: 10
        };
     });
   },
@@ -67,11 +71,10 @@ let Sunburst = BaseChart.extend({
     return {
       name: name,
       children: this.getChildren(children),
-      size: 10
+      size: 20
     };
   },
   getName: function (d) {
-
     return d.player ? d.player : d.coachName;
   },
   teamRosterLoaded: function (teamData, data) {
@@ -85,7 +88,7 @@ let Sunburst = BaseChart.extend({
   isAllDataLoaded: function () {
     this.totalLoaded += 1;
     if (this.totalLoaded !== this.nbaTeams.length) return;
-    console.log("this.this.dataNBA",this.dataNBA);
+    // console.log("this.this.dataNBA",this.dataNBA);
     this.formatDataD3(this.dataNBA);
     this.buildChart();
   },
@@ -104,7 +107,7 @@ let Sunburst = BaseChart.extend({
       .size([2 * Math.PI, this.size.radius]);
 
     this.root = d3.hierarchy(data)  // Find the Root Node
-      .sum(function (d) { return d.size});
+       .sum(function(d) { return !d.children || d.children.length === 0 ? d.size :0; });
 
     this.partition(this.root);  // Calculate each arc
 
@@ -118,9 +121,17 @@ let Sunburst = BaseChart.extend({
   computeTextRotation: function (d) {
     var angle = (d.x0 + d.x1) / Math.PI * 90;
 
-           // Avoid upside-down labels
-           return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
-           //return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
+      if ( d.depth === 1) {
+        angle = (angle < 180) ? angle - 90 : angle + 90;
+      } else if ( d.depth === 2) {
+        angle = (angle < 120 || angle > 270) ? angle : angle + 180;
+      } else if ( d.depth === 3) {
+        angle = (angle < 180) ? angle - 90 : angle + 90;
+      } else {
+        angle = (angle < 120 || angle > 270) ? angle : angle + 180;
+      }
+
+    return angle; // labels as spokes
   },
   buildChart: function () {
     const self = this;
@@ -128,15 +139,21 @@ let Sunburst = BaseChart.extend({
 
     this.svg.selectAll('g')
       .data(this.root.descendants())
-      .enter().append('g').attr("class", "node").append('path')
+      .enter().append('g').attr("class", "node")
+      .append('path')
       .attr("display", function (d) { return d.depth ? null : "none"; })
       .attr("d", self.arc)
       .style('stroke', '#fff')
       .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); });
 
     this.svg.selectAll(".node")  // add Labels for each node
+      .each(function () {
+        // console.log("Width: ", this);
+      })
       .append("text")
       .attr("transform", function(d) {
+        console.log("depth", d.depth);
+
           return "translate(" + self.arc.centroid(d) + ")rotate(" + self.computeTextRotation(d) + ")"; })
       .attr("dx", "-20")
       .attr("dy", ".5em")
